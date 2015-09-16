43414b9be9cadf595ee01d44c496365d0a29725b
IosuR <irecaldema14dw@ikzubirimanteo.com>
2015-09-16 06:36:36 +0000

    0.02

--- php/conexionPDO.php
+++ php/conexionPDO.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();
     $servername = "localhost";
     $usuario = "zubiri";
     $password = "";
@@ -7,10 +8,12 @@
         try {
     	    $conn = new PDO('mysql:host=localhost;dbname=TODO_BD', $usuario, $password);
     	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    	    echo "Conexion establecida"; 
+    	    //echo "Conexion establecida"; 
+    	    return true;
         }
         catch(PDOException $e){
             echo "Connection failed: " . $e->getMessage();
+            return false;
         }
     }
 ?>
--- php/login.php
+++ php/login.php
@@ -1,14 +1,26 @@
 <?php
 include("conexionPDO.php");
-    if (isset($_SESSION['usuario'])){
-        header("location:home.php");
+session_start();
+$conn = conexionPDO();
+    if ($conn){
+        //busqueda en la base de datos del usuario
+        //comprobacion de contraseña
+        //creacion de variables de sesion
+        //si todo ok se cambia a la pagina principal
+        if ($_POST){
+            $nombre=$_POST["nombre"];
+            $pass=$_POST["contrasena"];
+            $sql="select nombre from usuarios where userid="+$usuario+"and pass="+$pass;
+            $nombre=mysql_query($sql,$link);
+            if ($userid!=null){
+                $_SESSION['usuario']=$userid;
+                $_SESSION['nombre']=$nombre;
+                header("location:home.php");
+            }
+        }
     } else {
-        session_start();
-        $conn = conexionPDO();
-        $_SESSION['usuario'] = $usuario;
-        $_SESSION['conection'] = "ok";
+        //error en la conexion
     }
-   
 ?>
 <!DOCTYPE html>
 <html>
@@ -17,9 +29,9 @@ include("conexionPDO.php");
         <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
     </head>
     <body>
-        <form action="conexionPDO.php">
-            nombre:
-            <input type="text" name="nombre"/>
+        <form action="" method="post">
+            usuario:
+            <input type="text" name="usuario"/>
             contraseña:
             <input type="text" name="contrasena"/>
             <input type="submit" value="login"/>
